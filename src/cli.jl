634f13b1e16cb6509ca8e7ac028669439d788060
Kavi Gupta <kavi@berkeley.edu>
2023-10-02 17:45:17 -0400

    update cli

--- src/cli.jl
+++ src/cli.jl
@@ -1,12 +1,25 @@
 using Stitch
+using ArgParse
+
 import Stitch as S
 import JSON
 
+
 function cli()
+    # parse argument --iterations
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "--iterations"
+            help="Number of iterations to run"
+            default=1
+            arg_type=Int
+    end
+
+    args = parse_args(s)
     line = readline()
     json = JSON.parse(line)
     corpus = Corpus([Program(parse(SExpr, p), i, i) for (i, p) in enumerate(json)])
-    result = compress(corpus)
+    result = compress(corpus, iterations=args["iterations"])
     println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     println(JSON.json(result))
 end
