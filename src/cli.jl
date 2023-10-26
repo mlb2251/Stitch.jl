19830baa9f8a87997ee23384045f183a921cfe91
Kavi Gupta <kavi@berkeley.edu>
2023-10-26 15:02:39 -0400

    switch to compressing iteratively

--- src/cli.jl
+++ src/cli.jl
@@ -10,23 +10,34 @@ function cli()
     s = ArgParseSettings()
     @add_arg_table s begin
         "--iterations"
-            help="Number of iterations to run"
-            default=1
-            arg_type=Int
+        help = "Number of iterations to run"
+        default = 1
+        arg_type = Int
     end
 
     @add_arg_table s begin
         "--max-arity"
-            help="Maximum arity of abstractions"
-            default=2
-            arg_type=Int
+        help = "Maximum arity of abstractions"
+        default = 2
+        arg_type = Int
+    end
+
+    @add_arg_table s begin
+        "--dfa"
+        help = "DFA to use for parsing"
+        arg_type = String
     end
 
     args = parse_args(s)
     line = readline()
     json = JSON.parse(line)
     corpus = Corpus([Program(parse(SExpr, p), i, i) for (i, p) in enumerate(json)])
-    abstractions, corpus = compress(corpus, iterations=args["iterations"], max_arity=args["max-arity"])
+    abstractions, corpus = compress_imperative(
+        corpus,
+        args["dfa"],
+        iterations=args["iterations"],
+        max_arity=args["max-arity"]
+    )
     println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     println(JSON.json([
         Dict([("arity", abstraction.arity), ("sym_arity", abstraction.sym_arity), ("body", string(abstraction.body))])
