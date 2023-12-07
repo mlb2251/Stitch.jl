1963e4ce6ec2b1fa24fecdcff7baa23bfedb88fc
Kavi Gupta <kavi@berkeley.edu>
2023-12-07 15:44:54 -0500

    Internalize symbols (#17)
    * internalize symbols
    * remove new_symbol
    
    basically: Now we dont do the whole DFA matching within expansion, we do it ahead of time

--- src/search.jl
+++ src/search.jl
@@ -49,6 +49,7 @@ Base.show(io::IO, obj::SyntacticNodeExpansion) = pretty_show(io, obj; indent=fal
 struct AbstractionExpansion <: Expansion
     index::Int
     fresh::Bool
+    dfa_state::Symbol
 end
 
 Base.show(io::IO, obj::AbstractionExpansion) = pretty_show(io, obj; indent=false)
@@ -62,6 +63,7 @@ Base.show(io::IO, obj::ContinuationExpansion) = pretty_show(io, obj; indent=fals
 struct SymbolExpansion <: Expansion
     idx::Int
     fresh::Bool
+    dfa_state::Symbol
 end
 
 Base.show(io::IO, obj::SymbolExpansion) = pretty_show(io, obj; indent=false)
