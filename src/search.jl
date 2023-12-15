537ce5fad1fe9c1c5534d755f2d06018bfb792ba
Kavi Gupta <kavi@berkeley.edu>
2023-12-14 20:35:08 -0500

    fix bugs

--- src/search.jl
+++ src/search.jl
@@ -374,11 +374,15 @@ function stitch_search(corpus, config)
         expansion = pop!(search_state.expansions)
 
         # upper bound check
-        if config.upper_bound_fn(search_state, expansion) <= search_state.best_util
+        b = config.upper_bound_fn(search_state, expansion)
+        if b <= search_state.best_util
             is_tracked_pruned(search_state, expansion=expansion, message="$(@__FILE__):$(@__LINE__) - upper bound $(config.upper_bound_fn(search_state,expansion)) <= best util $(search_state.best_util)")
             plot && push!(plot_data.pruned_bound, (search_state.stats.expansions, config.upper_bound_fn(search_state, expansion)))
             continue # skip - worse than best so far
         end
+
+        # print out body, num matches, bound
+        printstyled("EXPAND: ", search_state.abstraction.body, " with ", length(search_state.matches), " matches and bound ", b, "\n", color=:blue)
 
         # do the expansion
         expand_general!(search_state, expansion)
