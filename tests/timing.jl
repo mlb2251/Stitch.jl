570977791a8ec1d65defe1e54dfedde81c3d8f74
Kavi Gupta <kavi@berkeley.edu>
2024-01-10 13:44:33 -0500

    improve timing script

--- tests/timing.jl
+++ tests/timing.jl
@@ -3,15 +3,44 @@ using Stitch
 using Test
 using Statistics
 using JSON
+using ArgParse
 
 include("./framework.jl")
+
+run(s) = integrate(s, "$s-out.json")
+
+s = ArgParseSettings()
+@add_arg_table s begin
+    "--folders"
+    help = "Folders to use"
+    nargs = '+'
+end
+
+@add_arg_table s begin
+    "--paths"
+    help = "Paths to use"
+    nargs = '+'
+end
+
+args = parse_args(s)
+
+function tests()
+    if length(args["folders"]) + length(args["paths"]) != 0
+        [folder_tests(folder) for folder in args["folders"]]
+        [run(path) for path in args["paths"]]
+    else
+        full_tests()
+    end
+end
 
 function fn_to_run()
     oldstd = stdout
     redirect_stdout(devnull)
-    t = @elapsed full_tests()
+    t = @elapsed tests()
     redirect_stdout(oldstd) # recover original stdout
+    # print t and then flush
     println(t)
+    flush(stdout)
     t
 end
 
