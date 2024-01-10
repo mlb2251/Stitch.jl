042bd43eaf33de5fc9473e12b0574e84541ec1ca
Kavi Gupta <kavi@berkeley.edu>
2024-01-10 18:42:59 -0500

    single call

--- src/expr.jl
+++ src/expr.jl
@@ -170,23 +170,25 @@ const global_struct_hash_no_symbol = Dict{HashNode,Int}()
 sets structural hash value, possibly with side effects of updating the structural hash, and
 sets e.metadata.struct_hash. Requires .metadata to be set so we know this will be used immutably
 """
-function struct_hash(e::SExpr; no_symbols=false)::Int
-    isnothing(e.metadata) || isnothing(e.metadata.struct_hash) || return no_symbols ? e.metadata.struct_hash_no_symbol : e.metadata.struct_hash
-
-    struct_hashtable = no_symbols ? global_struct_hash_no_symbol : global_struct_hash
+function struct_hash(e::SExpr)::Tuple{Int,Int}
+    isnothing(e.metadata) || return (e.metadata.struct_hash_no_symbol, e.metadata.struct_hash)
 
     leaf = e.leaf
 
-    if no_symbols && string(leaf)[1] == '&'
+    children = map(struct_hash, e.children)
+
+    node_w_symbol = HashNode(leaf, map(x -> x[2], children))
+    if string(leaf)[1] == '&'
         leaf = Symbol("&symbol")
     end
-
-    node = HashNode(leaf, map(x -> struct_hash(x; no_symbols=no_symbols), e.children))
-    if !haskey(struct_hashtable, node)
-        struct_hashtable[node] = length(struct_hashtable) + 1
+    node_wo_symbol = HashNode(leaf, map(x -> x[1], children))
+    if !haskey(global_struct_hash, node_w_symbol)
+        global_struct_hash[node_w_symbol] = length(global_struct_hash) + 1
     end
-    isnothing(e.metadata) || (e.metadata.struct_hash = struct_hashtable[node])
-    return struct_hashtable[node]
+    if !haskey(global_struct_hash_no_symbol, node_wo_symbol)
+        global_struct_hash_no_symbol[node_wo_symbol] = length(global_struct_hash_no_symbol) + 1
+    end
+    return (global_struct_hash_no_symbol[node_wo_symbol], global_struct_hash[node_w_symbol])
 end
 
 
