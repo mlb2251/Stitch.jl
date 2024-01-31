98ad2809696328de029e9a7ca04d266d744b166d
Kavi Gupta <kavi@berkeley.edu>
2024-01-31 18:31:31 -0500

    various updates

--- timing.py
+++ timing.py
@@ -1,31 +1,85 @@
-import time
+import shlex
+import sys
 import subprocess
+import argparse
 
 
-def run_and_gather_time(*cmd):
-    start = time.time()
-    subprocess.check_output(cmd)
-    end = time.time()
-    print("Time taken: ", end - start)
-    return end - start
+def arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--args", nargs="+", default=None)
+    parser.add_argument("--branches", nargs="+")
+    args = parser.parse_args()
+    if args.args is None:
+        args.args = [[]]
+    elif args.args == ["/imperative_realistic_each"]:
+        args.args = [
+            ["--paths", f"data/imperative_realistic/{i}.json"] for i in range(10)
+        ]
+    else:
+        args.args = [shlex.split(arg) for arg in args.args]
+    return args
 
 
-def run_several_times(*cmd, n=4):
-    times = []
-    for _ in range(n):
-        times.append(run_and_gather_time(*cmd))
-    return times
+def tee_cmd(cmd):
+    result = []
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for line in iter(p.stdout.readline, b""):
+        sys.stdout.write(line.decode())
+        sys.stdout.flush()
+        result.append(line.decode())
+    p.wait()
+    return "".join(result)
 
 
-def run_several_times_and_average(*cmd, n=4):
-    _, *times = run_several_times(*cmd, n=n)
-    avg = sum(times) / len(times)
-    # Times taken X, Y, Z; average: W
-    # round to 1 decimal place
-    times = [f"{t:.1f}" for t in times]
-    print(f"Times taken {', '.join(times)}; average: {avg:.1f}")
+def last_line(string):
+    return string.strip().split("\n")[-1]
 
 
-# julia --project=../Stitch.jl/ tests/integration.jl
+def median_time(string):
+    return float(last_line(string).split()[-1])
 
-run_several_times_and_average("julia", "--project=../Stitch.jl/", "tests/integration.jl")
+
+def print_comparison(branch1, branch2, output1, output2):
+    print()
+    print("*" * 80)
+    print(f"Time change from {branch1} to {branch2}")
+    for arg in output1:
+        if arg:
+            print(arg + ":")
+        print(f"Before: {last_line(output1[arg])}")
+        print(f"After: {last_line(output2[arg])}")
+        print(
+            f"Change: {median_time(output2[arg]) / median_time(output1[arg]) - 1:+.1%}"
+        )
+        print()
+    print("*" * 80)
+
+
+def main():
+    args = arguments()
+    branches = []
+    outputs = []
+    for branch in args.branches:
+        output = {}
+        for arg in args.args:
+            subprocess.check_call(["git", "checkout", branch])
+            print()
+            print("*" * 80)
+            print(branch, " ".join(arg))
+            result = tee_cmd(
+                [
+                    "julia",
+                    "--project=.",
+                    "tests/timing.jl",
+                    *arg,
+                ]
+            )
+            output[" ".join(arg)] = result
+        branches.append(branch)
+        outputs.append(output)
+        if len(branches) > 1:
+            print_comparison(branches[-2], branches[-1], outputs[-2], outputs[-1])
+
+
+if __name__ == "__main__":
+    main()
