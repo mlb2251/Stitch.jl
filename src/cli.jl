708b5fe8eca9b7490fb6beb37c269dcc1845b862
Kavi Gupta <kavi@berkeley.edu>
2024-01-31 18:32:27 -0500

    update

--- src/cli.jl
+++ src/cli.jl
@@ -69,20 +69,23 @@ function cli()
         args["dfa"],
         iterations=args["iterations"],
         max_arity=args["max-arity"],
+        match_sequences=true,
         size_by_symbol=size_by_symbol,
         application_utility_fixed=args["application-utility-fixed"],
         application_utility_metavar=args["application-utility-metavar"],
-        application_utility_symvar=args["application-utility-symvar"]
+        application_utility_symvar=args["application-utility-symvar"],
     )
     println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     println(JSON.json([
         Dict([
             ("arity", abstraction.arity),
             ("sym_arity", abstraction.sym_arity),
+            ("choice_arity", abstraction.choice_arity),
             ("body", string(abstraction.body)),
             ("dfa_root", abstraction.dfa_root),
             ("dfa_metavars", abstraction.dfa_metavars),
             ("dfa_symvars", abstraction.dfa_symvars),
+            ("dfa_choicevars", abstraction.dfa_choicevars),
         ])
         for abstraction in abstractions
     ]))
