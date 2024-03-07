82a4be9763a827ca9dbf82590ac63cec75d3a1fe
Kavi Gupta <kavi@berkeley.edu>
2024-03-07 16:40:07 -0500

    check same in context at end to prune more

--- src/expand.jl
+++ src/expand.jl
@@ -1129,29 +1129,34 @@ function choice_var_always_used_or_not(search_state)
 end
 
 function choice_var_always_used_or_not(search_state::SearchState{Match}, i)
-    first_match_length = length(search_state.matches[1].choice_var_captures[i])
-    if first_match_length > 1
-        # TODO this is necessary because metavariables can't match multiple things
-        # so in some cases choice variables are optimal even if always used
-        return false
-    end
-    if all(match -> length(match.choice_var_captures[i]) == first_match_length, search_state.matches)
+    first_match = search_state.matches[1]
+    first_match_choice = first_match.choice_var_captures[i]
+    if all(match -> compatible_captures(match, match.choice_var_captures[i], first_match, first_match_choice), search_state.matches)
         return true
     end
     return false
 end
 
 function choice_var_always_used_or_not(search_state::SearchState{MatchPossibilities}, i)
-    first_match_length = length(search_state.matches[1].alternatives[1].choice_var_captures[i])
-    if first_match_length > 1
-        # TODO this is necessary because metavariables can't match multiple things
-        # so in some cases choice variables are optimal even if always used
-        return false
-    end
-    if all(match_poss -> all(match -> length(match.choice_var_captures[i]) == first_match_length, match_poss.alternatives), search_state.matches)
+    first_match = search_state.matches[1].alternatives[1]
+    first_match_choice = first_match.choice_var_captures[i]
+    if all(match_poss -> all(match -> compatible_captures(match, match.choice_var_captures[i], first_match, first_match_choice), match_poss.alternatives), search_state.matches)
         return true
     end
     return false
+end
+
+function compatible_captures(m_a::Match, capture_a::Vector{SExpr}, m_b::Match, capture_b::Vector{SExpr})
+    # returns true iff capture_a and capture_b are compatible, i.e., they can be replaced by a metavariable
+    if length(capture_a) != length(capture_b)
+        return false
+    end
+    if length(capture_a) <= 1
+        # if there's 0 or 1 it can be replaced by a metavariable
+        return true
+    end
+    # check if they are all the same up to renaming
+    return all(i -> same_in_context(m_a, m_b, capture_a[i], capture_b[i]), 1:length(capture_a))
 end
 
 function variables_at_front_of_root_sequence(search_state)
