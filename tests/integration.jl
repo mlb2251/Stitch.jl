8afa6369a62eff501a45a93e0c99560435d1c99c
Kavi Gupta <kavi@berkeley.edu>
2024-07-16 12:39:02 -0400

    Revert "just run the one test"
    This reverts commit 196da6c19f211b601b3c6e259be6ecb537c58841.

--- tests/integration.jl
+++ tests/integration.jl
@@ -4,9 +4,7 @@ using Test
 import JSON
 
 include("./framework.jl")
-run(s) = integrate(s, "$s-out.json")
 
 is_testing = true
 
-# full_tests()
-run("data/imperative/subsequence-in-start-and-end-items.json")
+full_tests()
