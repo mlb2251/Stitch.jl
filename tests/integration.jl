196da6c19f211b601b3c6e259be6ecb537c58841
Kavi Gupta <kavi@berkeley.edu>
2024-07-16 12:34:46 -0400

    just run the one test

--- tests/integration.jl
+++ tests/integration.jl
@@ -4,7 +4,9 @@ using Test
 import JSON
 
 include("./framework.jl")
+run(s) = integrate(s, "$s-out.json")
 
 is_testing = true
 
-full_tests()
+# full_tests()
+run("data/imperative/subsequence-in-start-and-end-items.json")
