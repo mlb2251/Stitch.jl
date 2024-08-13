d08f5fefe36fcce7d9e23a67120a2d6630d6314d
Maddy Bowers <mlbowers@mit.edu>
2024-08-13 04:41:26 -0400

    tried lots of things. minor changes

--- src/smc/smc.jl
+++ src/smc/smc.jl
@@ -1,4 +1,5 @@
 import JSON
+import Random
 
 abstract type PExpr end
 
@@ -14,10 +15,10 @@ end
 #     body::PExpr
 # end
 
-struct Var <: PExpr
-    idx::Int
-    name::Symbol
-end
+# struct Var <: PExpr
+#     idx::Int
+#     name::Symbol
+# end
 
 struct MetaVar <: PExpr
     idx::Int
@@ -28,23 +29,42 @@ struct Prim <: PExpr
     name::Symbol
 end
 
-Base.copy(e::App) = App(copy(e.f), copy.(e.args))
+Base.copy(e::App) = App(copy(e.f), PExpr[copy(arg) for arg in e.args])
 # Base.copy(e::Abs) = Abs(e.argc, copy(e.argnames), copy(e.body))
-Base.copy(e::Var) = Var(e.idx, e.name)
+# Base.copy(e::Var) = Var(e.idx, e.name)
 Base.copy(e::MetaVar) = MetaVar(e.idx, e.name)
 Base.copy(e::Prim) = Prim(e.name)
 
+Base.:(==)(e1::App, e2::App) = e1.f == e2.f && e1.args == e2.args
+# Base.:(==)(e1::Abs, e2::Abs) = e1.argc == e2.argc && e1.argnames == e2.argnames && e1.body == e2.body
+# Base.:(==)(e1::Var, e2::Var) = e1.idx == e2.idx && e1.name == e2.name
+Base.:(==)(e1::MetaVar, e2::MetaVar) = e1.idx == e2.idx && e1.name == e2.name
+Base.:(==)(e1::Prim, e2::Prim) = e1.name == e2.name
+
+Base.hash(e::App, h::UInt) = hash(hash(e.f, hash(e.args, h)))
+# Base.hash(e::Abs, h::UInt) = hash(hash(e.argc, hash(e.argnames, hash(e.body, h)))
+# Base.hash(e::Var, h::UInt) = hash(hash(e.idx, hash(e.name, h)))
+Base.hash(e::MetaVar, h::UInt) = hash(hash(e.idx, hash(e.name, h)))
+Base.hash(e::Prim, h::UInt) = hash(hash(e.name, h))
 
 
 struct Production
     head::PExpr
     argc::Int
+    hash::UInt
 end
-Production(e::App) = Production(e.f, length(e.args))
+Production(e::App) = Production(e.f, length(e.args), hash(e.f, hash(length(e.args))))
 # Production(e::Abs) = error("there is no production for an abstraction")
-Production(e::Var) = Production(e, 0)
-Production(e::MetaVar) = Production(e, 0)
-Production(e::Prim) = Production(e, 0)
+# Production(e::Var) = Production(e, 0)
+Production(e::MetaVar) = error("there is no production for a metavar")
+Production(e::Prim) = Production(e, 0, hash(e))
+
+Base.copy(p::Production) = Production(copy(p.head), p.argc, p.hash)
+
+Base.isequal(p1::Production, p2::Production) = p1.hash == p2.hash && p1.argc == p2.argc && p1.head == p2.head
+
+Base.hash(p::Production, h::UInt) = hash(hash(p.head, hash(p.argc, h)))
+
 
 
 const NONAME::Symbol = :noname
@@ -71,6 +91,7 @@ end
 # or boop level
 struct CorpusNode
     expr::PExpr
+    expr_hash::UInt # NON-unique hash
     children::Vector{CorpusNode}
     # children_expr_paths::Vector{Path}
     production::Production
@@ -88,20 +109,21 @@ end
 
 function Corpus(exprs::Vector{PExpr})
     programs = ProgramInfo.(1:length(exprs))
-    nodes = [CorpusNode(expr, program) for (expr, program) in zip(exprs, programs)]
+    nodes = [make_corpus_nodes(expr, program) for (expr, program) in zip(exprs, programs)]
     return Corpus(programs, nodes)
 end
 
-function CorpusNode(expr::PExpr, program::ProgramInfo)
+function make_corpus_nodes(expr::PExpr, program::ProgramInfo)
     # if expr isa Abs
     #     # we skip over lambdas because you cant match at them
     #     return CorpusNode(expr.body, program)
     # end
-    node = CorpusNode(expr, CorpusNode[], Production(expr), program)
+
+    node = CorpusNode(expr, hash(expr), CorpusNode[], Production(expr), program)
     if expr isa App
         # we dont do `f` - matching at `f` is not eta long
         for arg in expr.args
-            push!(node.children, CorpusNode(arg, program))
+            push!(node.children, make_corpus_nodes(arg, program))
         end
     end
     return node
@@ -126,7 +148,16 @@ end
 
 const Path = Vector{Int}
 
+
+# struct Match
+#     node::CorpusNode
+#     metavar_args::Vector{CorpusNode}
+# end
+
+# Base.copy(m::Match) = Match(m.node, copy(m.metavar_args)) # can keep shallow
+
 mutable struct Abstraction
+    # matches::Vector{Match}
     matches::Vector{CorpusNode}
     metavar_paths::Vector{Path}
     expr::PExpr
@@ -135,12 +166,13 @@ mutable struct Abstraction
     utility::Float64
 end
 
-Base.copy(a::Abstraction) = Abstraction(copy(a.matches), copy.(a.metavar_paths), copy(a.expr), a.fresh_metavar, a.size, a.utility)
+# Base.copy(a::Abstraction) = Abstraction(Match[copy(m) for m in a.matches], copy.(a.metavar_paths), copy(a.expr), a.fresh_metavar, a.size, a.utility)
+Base.copy(a::Abstraction) = Abstraction(copy(a.matches), Path[copy(p) for p in a.metavar_paths], copy(a.expr), a.fresh_metavar, a.size, a.utility)
 
 
 function getchild(node::CorpusNode, path::Path)::CorpusNode
     for i in path
-        node = node.children[i]
+        @inbounds node = node.children[i]
     end
     return node
 end
@@ -182,21 +214,33 @@ function sample_expansion!(abs::Abstraction)
     path = popat!(abs.metavar_paths, i)
     # pick a random match location to use as the basis for expansion
     match = abs.matches[rand(1:end)]
-    
+
     prod = getchild(match, path).production
+    # prod = match.metavar_args[i].production
+
+    # @show abs
 
     # subset to the matches
     filter!(abs.matches) do node
-        getchild(node, path).production == prod
+        # if match.metavar_args[i].production == prod
+        #     arg = popat!(match.metavar_args, i);
+        #     for j in 1:prod.argc
+        #         push!(match.metavar_args, arg.children[j])
+        #     end
+        #     return true
+        # end
+        # return false
+        prod2 = getchild(node, path).production
+        prod2.hash == prod.hash
     end
 
     if prod.argc > 0
         new_expr = App(prod.head, PExpr[])
-        for i in 1:prod.argc
+        for j in 1:prod.argc
             push!(new_expr.args, MetaVar(abs.fresh_metavar, metavar_names[abs.fresh_metavar]))
             abs.fresh_metavar += 1
             new_path = copy(path)
-            push!(new_path, i)
+            push!(new_path, j)
             push!(abs.metavar_paths, new_path)
         end
     else
@@ -211,6 +255,7 @@ end
 
 function identity_abstraction(corpus)
     nodes = descendants(corpus)
+    # return Abstraction([Match(node, CorpusNode[node]) for node in nodes], Path[Int[]], MetaVar(1, metavar_names[1]), 2, 0, 0.)
     return Abstraction(nodes, Path[Int[]], MetaVar(1, metavar_names[1]), 2, 0, 0.)
 end
 
@@ -236,21 +281,27 @@ mutable struct Particle
 end
 Base.copy(p::Particle) = Particle(copy(p.abs), p.weight, p.done)
 
-function test(; path="data/cogsci/nuts-bolts.json")
+function test(; path="data/cogsci/nuts-bolts.json", seed=nothing)
     programs = String.(JSON.parsefile(path))
     corpus = Corpus(programs)
-    num_particles = 300
+    num_particles = 3000
 
     init_abs = identity_abstraction(corpus)
     init_particle = Particle(init_abs, 0., false)
     particles = Particle[copy(init_particle) for _ in 1:num_particles]
 
+    # tmp_particles = Particle[copy(init_particle) for _ in 1:num_particles]
+
 
     best_utility = 0.
     temperature = .5
 
+    !isnothing(seed) && Random.seed!(seed)
+    # @show Random.seed!()
+
     while any(p -> !p.done, particles)
         for particle in particles
+            # println("expand")
             res = sample_expansion!(particle.abs)
             particle.done = !res
             if particle.abs.utility > best_utility
@@ -258,6 +309,8 @@ function test(; path="data/cogsci/nuts-bolts.json")
                 println("new best: ", particle.abs)
             end
         end
+
+        # println("resample")
 
         # resample
         for particle in particles
@@ -272,22 +325,30 @@ function test(; path="data/cogsci/nuts-bolts.json")
             break
         end
         weights ./= sum(weights)
-        # @show [exp(p.logweight/temperature) for p in particles]
-        particles = [copy(particles[sample(weights)]) for _ in 1:num_particles]
+
+        # new_idxs = Int[sample_normalized(weights) for _ in 1:num_particles]
+
+        # for (i, idx) in enumerate(new_idxs)
+        # #     @inbounds copy!(tmp_particles[i], particles[idx])
+        #     particles[i] = copy(particles[idx])
+        # end
+        # particles, tmp_particles = tmp_particles, particles
+
+        @inbounds particles = [copy(particles[sample_normalized(weights)]) for _ in 1:num_particles]
     end
 
 end
 
 
 """
-adapted from StatsBase.sample(); samples an index from `weights`
-with probability proportional to the weight at that index
+adapted from StatsBase.sample();
+this verison requires normalized weights (uncomment the first line sum to work with unnormalized weights)
 """
-function sample(weights)
-    t = rand() * sum(weights)
+function sample_normalized(weights)
+    t = rand() # * sum(w -> w, weights) 
     n = length(weights)
     i = 1
-    cw = weights[1]
+    @inbounds cw = weights[1]
     while cw < t && i < n
         i += 1
         @inbounds cw += weights[i]
@@ -362,19 +423,22 @@ function parse_expr_inner(tokens, env::Vector{Symbol})
     elseif token_sym ∈ env
         # Parse a var by name like "foo"
         idx = findfirst(x -> x === token_sym, env) # shadowing
-        return Var(idx, token_sym), tokens[2:end]
+        @assert false
+        # return Var(idx, token_sym), tokens[2:end]
     elseif token[1] == '#'
         # parse debruijn index #4
         idx = parse(Int, token[2:end])
         @assert idx > 0 "need one-index debruijn"
-        return Var(idx, get!(env, idx, NONAME)), tokens[2:end]
+        @assert false
+        # return Var(idx, get!(env, idx, NONAME)), tokens[2:end]
     elseif '#' ∈ token
         # variable combining name and debruijn like x#4
         parts = split(token, "#")
         name = Symbol(parts[1])
         idx = parse(Int, parts[2])
         @assert parts[1] == env[idx] "debruijn index must match variable name"
-        return Var(idx, name), tokens[2:end]
+        @assert false
+        # return Var(idx, name), tokens[2:end]
     else
         return Prim(token_sym), tokens[2:end]
     end
@@ -399,7 +463,7 @@ end
 #     end
 #     print(io, "-> ", e.body, ")")
 # end
-Base.show(io::IO, e::Var) = print(io, e.name)
+# Base.show(io::IO, e::Var) = print(io, e.name)
 Base.show(io::IO, e::Prim) = print(io, e.name)
 Base.show(io::IO, e::MetaVar) = printstyled(io, e.name; color=(e.idx%7)+1, bold=true)
 
