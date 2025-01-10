1b3fbb1ce0dd10bad5c50b99bae0eec0e07fd1ac
Kavi Gupta <kavi@berkeley.edu>
2025-01-09 22:07:29 -0500

    update

--- src/Stitch.jl
+++ src/Stitch.jl
@@ -1,7 +1,6 @@
 module Stitch
 
-export SExpr, stitch_search, Corpus, Program, compress, load_corpus, load_dfa, rewrite_novel, intermediate_search_results, SearchConfig
-export bottom_up_utility, SearchState, is_leaf, is_hole, find_holes, compute_best_score_and_lower_bound, bounds_analysis
+export SExpr, stitch_search, Corpus, Program, compress, load_corpus, load_dfa, rewrite_novel, bounds_analysis
 
 using AutoHashEquals
 
